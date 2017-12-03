c303d87b5997ce7639c6a16afa06568f9857a56f
Stefan Schuh <stefan.schuh.ba@gmail.com>
2017-12-03 19:29:26 +0100

    added CTL_L(KC_ENT) and moved RESET

--- keyboards/atomic/keymaps/sonja/keymap.c
+++ keyboards/atomic/keymaps/sonja/keymap.c
@@ -100,11 +100,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [QW] = { /* QWERTZ */
-  { KC_ESC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, KC_BSPC, KC_BSPC  },
-  { KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE,   DE_PLUS, KC_INS,  KC_DEL   },
-  { KC_ESC,  DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE,   KC_ENT,  KC_ENT,  KC_PGUP  },
-  { KC_LSFT, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN  },
-  { KC_LCTL, KC_LGUI, XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, SPC_FN,  SPC_FN,  XXXXXXX, XXXXXXX, RALT_HOM,KC_END,  KC_LEFT, KC_DOWN, KC_RGHT  },
+  { KC_ESC,       DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, KC_BSPC, KC_BSPC  },
+  { KC_TAB,       DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE,   DE_PLUS, KC_INS,  KC_DEL   },
+  { KC_ESC,       DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE,   KC_ENT,  KC_ENT,  KC_PGUP  },
+  { KC_LSFT,      DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN  },
+  {CTL_T(KC_ENT), KC_LGUI, XXXXXXX, KC_LALT, XXXXXXX, XXXXXXX, SPC_FN,  SPC_FN,  XXXXXXX, XXXXXXX, RALT_HOM,KC_END,  KC_LEFT, KC_DOWN, KC_RGHT  },
  },
 
 
@@ -124,16 +124,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [FN] = { /* Function Layer */
   { _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ___T___, ___T___  },
-  { _______, _______, _______, C(KC_F3),_______, C(KC_F5),_______, C(KC_F7),C(KC_F8),_______, _______, _______, _______, _______, _______  },
+  { _______, _______, _______, C(KC_F3),_______, C(KC_F5),_______, C(KC_F7),C(KC_F8),_______, _______, _______, _______, _______, MO(SYS)  },
   { _______, _______, _______, _______, _______, _______, _______, DE_LBRC, DE_RBRC, DE_LCBR, DE_RCBR, _______, ___T___, ___T___, _______  },
-  { MO(SYS), DE_PIPE, _______, _______, DE_LESS, DE_MORE, _______, DE_LPRN, DE_RPRN, _______, _______, _______, _______, _______, _______  },
+  { _______, DE_PIPE, _______, _______, DE_LESS, DE_MORE, _______, DE_LPRN, DE_RPRN, _______, _______, _______, _______, _______, _______  },
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  },
  },
  /* SYSTEM
  * .---------------------------------------------------------------------------------------------------------------------- 2u ------------.
  * |        |        |        |        |        |        |        |        |        |        |        |        |        | XXXXXX .        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |        |        |        |        | RESET  |        |        |        |        |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+- 2u ------------+--------|
  * |        |        |        |        |        |        |        |        |        |        |        |        | XXXXXX .        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+- 2u ---------------------+--------|
